01589c13bd30c014056d08d62e0ad2ee57258982
Arcadian420 <kaclhhdo@gmail.com>
2023-08-18 18:08:22 +0200

    update

--- lua.lua
+++ lua.lua
@@ -7,3 +7,12 @@ local ReplicatedStorage = cloneref(game:GetService("ReplicatedStorage"))
 local LocalPlayer = cloneref(PlayerService.LocalPlayer)
 local Character = cloneref(LocalPlayer.Character)
 local Backpack = cloneref(LocalPlayer.Backpack)
+
+function randomString()
+	local length = math.random(10,20)
+	local array = {}
+	for i = 1, length do
+		array[i] = string.char(math.random(32, 126))
+	end
+	return table.concat(array)
+end
