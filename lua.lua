6efa7ee7f8f8ad2046f0fb41697190d8e6c36e47
Arcadian420 <142536276+Arcadian420@users.noreply.github.com>
2023-08-18 02:19:47 +0200

    Services/Variables

---
+++ lua.lua
@@ -0,0 +1,8 @@
+--Services
+local PlayerService = cloneref(game:GetService("Players"))
+local RunService = cloneref(game:GetService("RunService"))
+
+--Variable
+local LocalPlayer = cloneref(PlayerService.LocalPlayer)
+local Character = cloneref(LocalPlayer.Character)
+local Backpack = cloneref(LocalPlayer.Backpack)
