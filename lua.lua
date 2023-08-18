2741de51a24eecd1347f6dc0fd9528e5ddab7cdf
pbJbbbZbmbbfmffZp <kaclhhdo@gmail.com>
2023-08-18 05:40:59 +0200

    update

--- lua.lua
+++ lua.lua
@@ -1,8 +1,9 @@
 --Services
 local PlayerService = cloneref(game:GetService("Players"))
 local RunService = cloneref(game:GetService("RunService"))
+local ReplicatedStorage = cloneref(game:GetService("ReplicatedStorage"))
 
---Variable
+--Variables
 local LocalPlayer = cloneref(PlayerService.LocalPlayer)
 local Character = cloneref(LocalPlayer.Character)
 local Backpack = cloneref(LocalPlayer.Backpack)
