2581f9bbb81c4b94c1d1621b0ca9639f196ed63f
Arcadian420 <kaclhhdo@gmail.com>
2023-08-18 18:39:47 +0200

    update

--- lua.lua
+++ lua.lua
@@ -8,6 +8,14 @@ LocalPlayer = cloneref(PlayerService.LocalPlayer)
 Character = cloneref(LocalPlayer.Character)
 Backpack = cloneref(LocalPlayer.Backpack)
 
+for i,AllPlayers in pairs(PlayerService:GetChildren()) do
+    print(v)
+    end
+
+for i,AllCharacters in pairs(PlayerService.Character:GetDescendant()) do
+	print(v)
+end
+
 function randomString()
 	local length = math.random(10,20)
 	local array = {}
