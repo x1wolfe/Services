de2502442d97204ace81227efbd4a0b7ccf5fdb0
Arcadian420 <kaclhhdo@gmail.com>
2023-08-23 18:48:14 +0200

    Added InsertService

--- lua.lua
+++ lua.lua
@@ -3,6 +3,7 @@ PlayerService = cloneref(game:GetService("Players"))
 RunService = cloneref(game:GetService("RunService"))
 ReplicatedStorage = cloneref(game:GetService("ReplicatedStorage"))
 MarketplaceService = cloneref(game:GetService("MarketplaceService"))
+InsertService = cloneref(game:GetService("InsertService"))
 
 --Variables
 LocalPlayer = cloneref(PlayerService.LocalPlayer)
